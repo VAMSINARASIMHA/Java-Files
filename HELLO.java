d3d539178d985456f2270fcc8f5ce37aaafce76a
VAMSINARASIMHA <vamsinarsimhapalepu@gmail.com>
2025-05-22 13:28:55 +0530

    First program in Java

--- HELLO.java
+++ HELLO.java
@@ -1,6 +1,8 @@
-public class HELLO {
+public class HELLO //defines a class named HELLO. In Java, every program must be inside a class.
+{
 
-    public static void main (String[] args) {
-        System.out.println("Hello World");
+    public static void main (String[] args) //is the entry point of any Java application. It tells the JVM where to start executing the program. 
+    {
+        System.out.println("Hello World");  //prints the message to the console.
     }
 }
