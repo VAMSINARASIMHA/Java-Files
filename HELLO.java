9e48d2851e2239382ac359d43f7435d65bc0bdd2
VAMSINARASIMHA <vamsinarsimhapalepu@gmail.com>
2025-06-07 18:57:11 +0530

    del

--- HELLO.java
+++
@@ -1,8 +0,0 @@
-public class HELLO //defines a class named HELLO. In Java, every program must be inside a class.
-{
-
-    public static void main (String[] args) //is the entry point of any Java application. It tells the JVM where to start executing the program. 
-    {
-        System.out.println("Hello World");  //prints the message to the console.
-    }
-}
